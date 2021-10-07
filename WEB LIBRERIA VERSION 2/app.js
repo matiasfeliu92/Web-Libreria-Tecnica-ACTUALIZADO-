305347009fbcda032e8afbc4dd691683eefe9894
matiasfeliu92 <matumazparrote@gmail.com>
2021-10-07 12:02:55 -0300

    correcciones libreria tecnica 7 10 21

--- WEB LIBRERIA VERSION 2/app.js
+++ WEB LIBRERIA VERSION 2/app.js
@@ -1,4 +1,3 @@
-
 // == selectores ==
 const contenedorProductos = document.getElementById('contenedor-productos')
 const contenedorCarrito = document.getElementById('carrito-contenedor')
@@ -12,8 +11,8 @@ const carrito = []
 
 const mostrarProductos = (array) => {
     contenedorProductos.innerHTML = ''
-    
-    array.forEach( (producto) => {
+
+    array.forEach((producto) => {
         const div = document.createElement('div')
         div.classList.add('producto')
         div.innerHTML = `
@@ -22,9 +21,9 @@ const mostrarProductos = (array) => {
                     <p class="precioProducto">Precio: $${producto.precio}</p>
                     <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
         `
-        
+
         contenedorProductos.appendChild(div)
-    } )
+    })
 }
 
 mostrarProductos(stockProductos)
@@ -41,8 +40,8 @@ const agregarAlCarrito = (itemId) => {
         productoEnCarrito.cantidad++
     } else {
 
-        const producto = stockProductos.find( (prod) => prod.id === itemId)
-    
+        const producto = stockProductos.find((prod) => prod.id === itemId)
+
         carrito.push({
             id: producto.id,
             nombre: producto.nombre,
@@ -50,7 +49,7 @@ const agregarAlCarrito = (itemId) => {
             cantidad: 1
         })
     }
-    
+
 
     console.log(carrito)
     actualizarCarrito()
@@ -83,14 +82,14 @@ const actualizarCarrito = () => {
 
 const eliminarProducto = (itemId) => {
     const producto = carrito.find((prod) => prod.id === itemId)
-    
+
     producto.cantidad--
 
-    if (producto.cantidad === 0) {
-        const index = carrito.indexOf(producto)
-        carrito.splice(index, 1)
-    }
-   
+        if (producto.cantidad === 0) {
+            const index = carrito.indexOf(producto)
+            carrito.splice(index, 1)
+        }
+
     actualizarCarrito()
 }
 
@@ -104,29 +103,29 @@ const selectPrecios = document.getElementById('precios')
 const filtrar = () => {
     let valorFiltroTalles = selectFiltro.value
     let valorFiltroPrecios = selectPrecios.value
-    
+
     let arrayFiltrado = []
 
     if (valorFiltroTalles == 'all') {
         arrayFiltrado = stockProductos
     } else {
-        arrayFiltrado = stockProductos.filter( el => el.talle == selectFiltro.value) 
+        arrayFiltrado = stockProductos.filter(el => el.talle == selectFiltro.value)
     }
 
     if (valorFiltroPrecios == 1) {
-        arrayFiltrado = arrayFiltrado.filter( el => el.precio <= 5000)
+        arrayFiltrado = arrayFiltrado.filter(el => el.precio <= 5000)
     } else if (valorFiltroPrecios == 2) {
-        arrayFiltrado = arrayFiltrado.filter( el => el.precio >= 5000)
+        arrayFiltrado = arrayFiltrado.filter(el => el.precio >= 5000)
     }
 
     mostrarProductos(arrayFiltrado)
 
 }
 
-selectFiltro.addEventListener('change', ()=>{
+selectFiltro.addEventListener('change', () => {
     filtrar()
 })
-selectPrecios.addEventListener('change', ()=>{
+selectPrecios.addEventListener('change', () => {
     filtrar()
 })
 
@@ -142,5 +141,5 @@ const buscar = (search) => {
 
 buscador.addEventListener('input', () => {
     const search = buscador.value.trim().toLowerCase()
-    mostrarProductos( buscar(search) )  
+    mostrarProductos(buscar(search))
 })
